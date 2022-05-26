636ed2ac215f755acf1867ab3efccaecc9b257ef
syangl <liusiyangliu@126.com>
2022-05-26 18:32:15 +0800

    version 1.3

--- design/Trigger_codes.sql
+++ design/Trigger_codes.sql
@@ -1,26 +1,26 @@
 use library;
 
-delimiter //
-CREATE TRIGGER LoginTrig
-BEFORE INSERT ON 读者 
-FOR EACH ROW 
-BEGIN
-	IF (new.账户ID NOT IN (SELECT 账户ID FROM 账号)) THEN
-		INSERT INTO 账号(账户ID,身份) VALUES(new.账户ID,'读者');
-	END IF;
-END; //
-delimiter ;
+-- delimiter //
+-- CREATE TRIGGER LoginTrig
+-- BEFORE INSERT ON 读者 
+-- FOR EACH ROW 
+-- BEGIN
+-- 	IF (new.账户ID NOT IN (SELECT 账户ID FROM 账号)) THEN
+-- 		INSERT INTO 账号(账户ID,身份) VALUES(new.账户ID,'读者');
+-- 	END IF;
+-- END; //
+-- delimiter ;
 
-delimiter //
-CREATE TRIGGER LoginTrigMana
-BEFORE INSERT ON 管理员 
-FOR EACH ROW 
-BEGIN
-	IF (new.账户ID NOT IN (SELECT 账户ID FROM 账号)) THEN
-		INSERT INTO 账号(账户ID,身份) VALUES(new.账户ID,'管理员');
-	END IF;
-END; //
-delimiter ;
+-- delimiter //
+-- CREATE TRIGGER LoginTrigMana
+-- BEFORE INSERT ON 管理员 
+-- FOR EACH ROW 
+-- BEGIN
+-- 	IF (new.账户ID NOT IN (SELECT 账户ID FROM 账号)) THEN
+-- 		INSERT INTO 账号(账户ID,身份) VALUES(new.账户ID,'管理员');
+-- 	END IF;
+-- END; //
+-- delimiter ;
 
 
 DROP PROCEDURE IF EXISTS num_update ;
@@ -32,7 +32,7 @@ BEGIN
 			update 图书 set 库存数量 = 库存数量 + add_num where 图书ID = ID;
 		WHEN option_type = 2  THEN
 			update 借阅记录 set 借阅数量 = 借阅数量 + add_num where 借阅记录ID = ID;
-		ELSE 
+		WHEN option_type = 3  THEN
 			update 罚款记录 set 未还数量 = 未还数量 + add_num where 罚款记录ID = ID;
     END CASE;
 END //
